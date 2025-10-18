c977c4275043362bf6129742c03c834936f28b56
kipure <kseason@gmail.com>
2025-10-18 06:19:08 +0000

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 pages = {
     "menu": [
-        st.Page("pages/00_about.py", title="top"),
+        st.Page("pages/00_top.py", title="top"),
         st.Page("pages/01_image.py", title="画像"),
     ],
 }
